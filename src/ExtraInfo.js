6c36df11c9a08792aa1291513345489fea68b2c0
David Lord <eternalcat@gmail.com>
2020-03-23 21:40:21 +1000

    Stop ExtraInfo buttons sharing animation state

--- src/ExtraInfo.js
+++ src/ExtraInfo.js
@@ -5,9 +5,10 @@ class ExtraInfo extends React.Component {
   render() {
     if (this.props.show === true) {
       // extended version
+      // <button> keys prevent reuse which in Firefox leads to animations being continued between them
       return (
         <div className="extra-info-expanded">
-        <button className="extra-info-expanded-button" onClick={this.props.onClick}>X</button>
+        <button className="extra-info-expanded-button" onClick={this.props.onClick} key={0}>X</button>
         <h2>Credits</h2>
         This is built using <a href="https://reactjs.org">React</a>. The cute loading bar is from <a href="https://css-tricks.com/css3-progress-bars/">CSS Tricks</a>. The button you just clicked is adapted from <a href="https://fdossena.com/?p=html5cool/buttons/i.frag">Federico Dossena</a>.
         <h2>Why?</h2>
@@ -18,7 +19,7 @@ class ExtraInfo extends React.Component {
       // floaty hint
       return (
         <div className="extra-info-hiding">
-        <button className="extra-info-hiding-button" onClick={this.props.onClick}>More info</button>
+        <button className="extra-info-hiding-button" onClick={this.props.onClick} key={1}>More info</button>
         </div>
       );
     }
